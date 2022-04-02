7fd6d0223d260d0be674882056927eccd0409cef
laceratione <laceratione@gmail.com>
2022-04-02 15:37:26 +0500

    simple app

---
+++ src/main/java/com/example/spring/springbootsimple/Counter.java
@@ -0,0 +1,9 @@
+package com.example.spring.springbootsimple;
+
+public class Counter {
+    public static int count = 0;
+
+    public static void inc(){
+        count++;
+    }
+}
---
+++ src/main/java/com/example/spring/springbootsimple/SimpleController.java
@@ -0,0 +1,17 @@
+package com.example.spring.springbootsimple;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleController {
+
+    @GetMapping("/count")
+    public ResponseEntity<String> getCount() {
+        Counter.inc();
+        return ResponseEntity.ok("{\"counter\":\"" + Counter.count + "\"}");
+    }
+
+}
